7602dd06dca9d24690c6b1d2a26fe47bea74c901
Antônio <devantonio.fer@gmail.com>
2023-10-10 17:40:00 -0300

    starting project

--- united_code_lab/manage.py
+++ united_code_lab/manage.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
+ALLOWED_HOSTS = ["*"]
+
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,5 +19,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
