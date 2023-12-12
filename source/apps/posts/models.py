374bdf10586bd48fc184221cf2948904c719786f
William Santana <williamandrade1058@gmail.com>
2023-12-12 14:18:42 -0300

    add class in models.py

--- source/apps/posts/models.py
+++ source/apps/posts/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from datetime import timezone
 
-# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey()
+    title = models.CharField(max_length=25, verbose_name='Título do post')
+    description = models.CharField(max_length=255, verbose_name='Descrição do post')
+    comments = models.ForeignKey('Comment', on_delete=models.CASCADE)
+    language = models.CharField(max_length=25, verbose_name='Linguagem utilizada no post')
+    knowledge_level = models.IntegerField(verbose_name='Nível de conhecimento')
+
+    send_at = models.DateTimeField(auto_now_add=True, verbose_name='Data e horário de envio')
+    create_at = models.DateField('Create At', default=timezone.now)
+    is_hidden = models.BooleanField(default=False)
+    updated_at = models.DateTimeField('Update At', default=timezone.now)
+
+    slug = models.SlugField(max_length=50)
+
+    def __str__(self):
+        return f'{self.title}',  f'{self.author}'
+
+
+class Comment(models.Model):
+    author = models.ForeignKey()
+    description = models.CharField(
+        max_length=255, verbose_name='Descrição do comentário')
+    send_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.author}', f'{self.description}'
